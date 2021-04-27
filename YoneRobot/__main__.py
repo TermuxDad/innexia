79470e8bdec70dc7f2b0d2473d40a4ba50b5eb6d
TermuxDad <76565235+TermuxDad@users.noreply.github.com>
2021-04-27 23:05:54 +0530

    Update __main__.py

--- YoneRobot/__main__.py
+++ YoneRobot/__main__.py
@@ -3,7 +3,7 @@ import time
 import re
 from sys import argv
 from typing import Optional
-
+import random
 
 from YoneRobot import (
     ALLOW_EXCL,
@@ -48,6 +48,13 @@ from telegram.ext import (
 from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
 from telegram.utils.helpers import escape_markdown
 
+PMSTICKER = (
+    "CAACAgUAAxkBAAPKYIgucBfAsS8IXpMGx8OVH_fF6l8AAlYDAALH8EFU920sYwWt8fgfBA",
+    "CAACAgUAAxkBAAPLYIgucZF3mhR5DWV6GHmkCrEX3p8AAv4CAAJb5UBU_4xxomdSVBsfBA",
+    "CAACAgUAAxkBAAPMYIguccpODQLN-dG5DSTyw_3AxPcAAqMEAAL4gUFUh6PUJHeaxrEfBA",
+    "CAACAgUAAxkBAAPNYIgucg-hb6_jvAq-HNf5YcEV_44AAukEAAL3lUBURNXT_yQSpHIfBA",
+    "CAACAgUAAxkBAAPOYIgudG4D4wQp1NG9gIrJ_lSlCJYAAvYCAALFykFUVLEPt5ZWI9cfBA",
+)
 
 def get_readable_time(seconds: int) -> str:
     count = 0
@@ -81,22 +88,29 @@ PM_START_TEXT = """
 
 buttons = [
     [
-        InlineKeyboardButton(text="Help & Commands ❔", callback_data="help_back"),
+        InlineKeyboardButton(text="❔ Help & Commands ❔", callback_data="help_back"),
         
     ],
 
     [
         InlineKeyboardButton(
-            text="Support 🔧", url="t.me/Innexiasupport"
+            text="👥 Group", url="t.me/Innexiasupport"
         ),
         InlineKeyboardButton(
-            text="Search 🔎", switch_inline_query_current_chat=""
+            text="Channel 📢", url="t.me/InnexiaUpdates"
         ),
     ],
 
     [
+        InlineKeyboardButton(text="💬 Information", callback_data="yone_"),
         InlineKeyboardButton(
-            text="Add To Group 👥", url="t.me/Innexiabot?startgroup=true"
+            text="Search 🔎", switch_inline_query_current_chat=""
+        ),
+    ],
+    
+    [
+        InlineKeyboardButton(
+            text="➕ Add Me To A Group ➕", url="t.me/Innexiabot?startgroup=true"
         ),
     ],
 ]
@@ -214,6 +228,9 @@ def start(update: Update, context: CallbackContext):
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
         else:
+            chat_id = update.effective_chat.id
+            temp = (random.choice(PMSTICKER))
+            context.bot.send_sticker(chat_id, sticker=temp) #message.bot.send_sticker(chat.id,
             update.effective_message.reply_text(
                 PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(buttons),
@@ -357,16 +374,410 @@ def yone_about_callback(update, context):
     query = update.callback_query
     if query.data == "yone_":
         query.message.edit_text(
-            text=""" ℹ️ I'm *yone*, a powerful group management bot built to help you manage your group easily.
-                 \n❍ I can restrict users.
-                 \n❍ I can greet users with customizable welcome messages and even set a group's rules.
-                 \n❍ I have an advanced anti-flood system.
-                 \n❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
-                 \n❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
-                 \n❍ I check for admins' permissions before executing any command and more stuffs
-                 \n\n_yone's licensed under the GNU General Public License v3.0_
-                 \nHere is the [💾Repository](https://github.com/noob-kittu/YoneRobot).
-                 \n\nIf you have any question about yone, let us know at .""",
+            text="""
+import importlib
+import time
+import re
+from sys import argv
+from typing import Optional
+import random
+
+from YoneRobot import (
+    ALLOW_EXCL,
+    CERT_PATH,
+    DONATION_LINK,
+    LOGGER,
+    OWNER_ID,
+    PORT,
+    SUPPORT_CHAT,
+    TOKEN,
+    URL,
+    WEBHOOK,
+    SUPPORT_CHAT,
+    dispatcher,
+    StartTime,
+    telethn,
+    pbot,
+    updater,
+)
+
+# needed to dynamically load modules
+# NOTE: Module order is not guaranteed, specify that in the config file!
+from YoneRobot.modules import ALL_MODULES
+from YoneRobot.modules.helper_funcs.chat_status import is_user_admin
+from YoneRobot.modules.helper_funcs.misc import paginate_modules
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+from telegram.error import (
+    BadRequest,
+    ChatMigrated,
+    NetworkError,
+    TelegramError,
+    TimedOut,
+    Unauthorized,
+)
+from telegram.ext import (
+    CallbackContext,
+    CallbackQueryHandler,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+)
+from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
+from telegram.utils.helpers import escape_markdown
+
+PMSTICKER = (
+    "CAACAgUAAx0CUg8vTgACB7tgcFxdQdBRT31liVCdyjEtHYv4jAACmwEAAgkoiVcvcpJLqZntIh4E",
+    "CAACAgUAAx0CUg8vTgACB7pgcFxX3vdP51uRelaWPTaXuqcpxQACQAcAAoKseVcHYeawlmWvnB4E",
+    "CAACAgUAAx0CUg8vTgACB7hgcFxKXFLZ284wzEvDL9dwQ_meOAACTwIAApikgFf5-oSv_YQKSx4E",
+    "CAACAgUAAx0CUg8vTgACB7dgcFxEIWI0fifJkNv31nAxnX1wuQACBAMAAt-BgFcTrzjWiAKkmR4E",
+    "CAACAgUAAx0CUg8vTgACB7ZgcFxACy9zAkFHFrosn5BCrsS-bAACmQEAAoUVgFeFCMfdgp5cZR4E",
+    "CAACAgUAAx0CUg8vTgACB7VgcFw6ysxUmyU2BH_ntbfm5BWCLQAClQIAAjObgVfRH1vLpxLA7h4E",
+    "CAACAgUAAx0CUg8vTgACB7RgcFw0ywAB0BoZ9VbL0yLXDq--OFoAAjQCAAKHnIFX4Zn36f5U7-MeBA",
+    "CAACAgUAAx0CUg8vTgACB7NgcFwtCwcvxLDKjfYpqdcyEP1z9QAC3wEAAmOLgFd5a4AAATm0TZQeBA",
+    "CAACAgUAAx0CUg8vTgACB7JgcFwmrwdnd--fvTbpBWNTmkHxewACjwEAAklgiFfuKUY41tIg4R4E",
+    "CAACAgUAAx0CUg8vTgACB7FgcFwfh1YJaUMngm2C1W5S91efRAACOwIAAsnDgVeLgOnjl6XUKh4E",
+    "CAACAgUAAx0CUg8vTgACB7BgcFwZdfyKtyXf0_4UBcsYPRn57wACuQEAAvbyiFcH3UXUREHtPh4E",
+    "CAACAgUAAx0CUg8vTgACB69gcFwQSD-mDqJCJEAm9--S7o19wQACRwMAAkOwiFftE8BncwWXax4E",
+    "CAACAgUAAx0CUg8vTgACB65gcFwL3dsSYp4lfY3Ux4MV_shsLgACJgIAAnZ7iFcBHt-vLHUcPB4E",
+    "CAACAgUAAx0CUg8vTgACB61gcFwGTXv3_SSpzopj4Zbkj1UbfAAC-wEAAh3niFf-lC97RzmTFB4E",
+    "CAACAgUAAx0CUg8vTgACB6tgcFwAATva875BFsNt5XDuj4mBirgAAuQBAAKDvHBXnVxT9y9HXekeBA",
+    "CAACAgUAAx0CUg8vTgACB6pgcFv5oJR7IO_4EuI_Mwcd1ci3EgACAwIAAsi_eVd-2vOORe7abh4E",
+    "CAACAgUAAx0CUg8vTgACB6lgcFvzBXqAV-bYRWI_ah-gghV8CQACMgIAAlMNeVeedJLMoB_xmx4E",
+    "CAACAgUAAxkBAAIIIGBwrr0qOED2DM67hmhUAQsgUHH5AALlAgAC-lWBVyoPto-smrNtHgQ",
+    "CAACAgUAAx0CUg8vTgACExpgf8JyNA7dwKOfo-0JO3cKbI_OiwACkQIAAtrKqVf9QV362ZEdRx8E",
+    "CAACAgUAAx0CUg8vTgACExtgf8J2mkcfngnZYXixRdBLDQg33gACpAEAAkTQyFdvHNLzM5185B8E",
+    "CAACAgUAAx0CUg8vTgACExxgf8J6_PbnCalkH-xMNZQyCMbaBwACEQIAAnySyFexxpDK2JGB7B8E",
+    "CAACAgUAAx0CUg8vTgACEx1gf8J9i_6fIXxHviAfl4nXt_mS1gACBwMAAmNMyFfDXfqJTIi-4x8E",
+)
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+
+PM_START_TEXT = """
+`Heya` 🤗 `I am` *INNEXIA* `your group super bot`
+`I am very fast and  more efficient  I provide awesome  features which a owner will look for  filter ,warn system,note keeping system flood!`
+"""
+
+buttons = [
+    [
+        InlineKeyboardButton(text="❔ Help & Commands ❔", callback_data="help_back"),
+        
+    ],
+
+    [
+        InlineKeyboardButton(
+            text="👥 Group", url="t.me/Innexiasupport"
+        ),
+        InlineKeyboardButton(
+            text="Channel 📢", url="t.me/InnexiaUpdates"
+        ),
+    ],
+
+    [
+        InlineKeyboardButton(text="💬 Information", callback_data="yone_"),
+        InlineKeyboardButton(
+            text="Search 🔎", switch_inline_query_current_chat=""
+        ),
+    ],
+    
+    [
+        InlineKeyboardButton(
+            text="➕ Add Me To A Group ➕", url="t.me/Innexiabot?startgroup=true"
+        ),
+    ],
+]
+
+
+HELP_STRINGS = """
+*SETTINGS*
+`Click on the buttons below to get documentation about specific modules..`"""
+
+
+
+
+DONATE_STRING = """Heya, glad to hear you want to donate!
+ You can support the project via [Paypal](#) or by contacting @kittu5588 \
+ Supporting isnt always financial! \
+ Those who cannot provide monetary support are welcome to help us develop the bot at ."""
+
+IMPORTED = {}
+MIGRATEABLE = []
+HELPABLE = {}
+STATS = []
+USER_INFO = []
+DATA_IMPORT = []
+DATA_EXPORT = []
+CHAT_SETTINGS = {}
+USER_SETTINGS = {}
+
+for module_name in ALL_MODULES:
+    imported_module = importlib.import_module("YoneRobot.modules." + module_name)
+    if not hasattr(imported_module, "__mod_name__"):
+        imported_module.__mod_name__ = imported_module.__name__
+
+    if imported_module.__mod_name__.lower() not in IMPORTED:
+        IMPORTED[imported_module.__mod_name__.lower()] = imported_module
+    else:
+        raise Exception("Can't have two modules with the same name! Please change one")
+
+    if hasattr(imported_module, "__help__") and imported_module.__help__:
+        HELPABLE[imported_module.__mod_name__.lower()] = imported_module
+
+    # Chats to migrate on chat_migrated events
+    if hasattr(imported_module, "__migrate__"):
+        MIGRATEABLE.append(imported_module)
+
+    if hasattr(imported_module, "__stats__"):
+        STATS.append(imported_module)
+
+    if hasattr(imported_module, "__user_info__"):
+        USER_INFO.append(imported_module)
+
+    if hasattr(imported_module, "__import_data__"):
+        DATA_IMPORT.append(imported_module)
+
+    if hasattr(imported_module, "__export_data__"):
+        DATA_EXPORT.append(imported_module)
+
+    if hasattr(imported_module, "__chat_settings__"):
+        CHAT_SETTINGS[imported_module.__mod_name__.lower()] = imported_module
+
+    if hasattr(imported_module, "__user_settings__"):
+        USER_SETTINGS[imported_module.__mod_name__.lower()] = imported_module
+
+
+# do not async
+def send_help(chat_id, text, keyboard=None):
+    if not keyboard:
+        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
+    dispatcher.bot.send_message(
+        chat_id=chat_id,
+        text=text,
+        parse_mode=ParseMode.MARKDOWN,
+        disable_web_page_preview=True,
+        reply_markup=keyboard,
+    )
+
+
+@run_async
+def test(update: Update, context: CallbackContext):
+    # pprint(eval(str(update)))
+    # update.effective_message.reply_text("Hola tester! _I_ *have* `markdown`", parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_text("This person edited a message")
+    print(update.effective_message)
+    
+
+@run_async
+def start(update: Update, context: CallbackContext):
+    args = context.args
+    uptime = get_readable_time((time.time() - StartTime))
+    if update.effective_chat.type == "private":
+        if len(args) >= 1:
+            if args[0].lower() == "help":
+                send_help(update.effective_chat.id, HELP_STRINGS)
+            elif args[0].lower().startswith("ghelp_"):
+                mod = args[0].lower().split("_", 1)[1]
+                if not HELPABLE.get(mod, False):
+                    return
+                send_help(
+                    update.effective_chat.id,
+                    HELPABLE[mod].__help__,
+                    InlineKeyboardMarkup(
+                        [[InlineKeyboardButton(text="⬅️ BACK", callback_data="help_back")]]
+                    ),
+                )
+
+            elif args[0].lower().startswith("stngs_"):
+                match = re.match("stngs_(.*)", args[0].lower())
+                chat = dispatcher.bot.getChat(match.group(1))
+
+                if is_user_admin(chat, update.effective_user.id):
+                    send_settings(match.group(1), update.effective_user.id, False)
+                else:
+                    send_settings(match.group(1), update.effective_user.id, True)
+
+            elif args[0][1:].isdigit() and "rules" in IMPORTED:
+                IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
+
+        else:
+            chat_id = update.effective_chat.id
+            temp = (random.choice(PMSTICKER))
+            context.bot.send_sticker(chat_id, sticker=temp) #message.bot.send_sticker(chat.id,
+            update.effective_message.reply_text(
+                PM_START_TEXT,
+                reply_markup=InlineKeyboardMarkup(buttons),
+                parse_mode=ParseMode.MARKDOWN,
+                timeout=60,
+            )
+    else:
+        update.effective_message.reply_text(
+            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+                uptime
+            ),
+            parse_mode=ParseMode.HTML,
+        )
+
+
+def error_handler(update, context):
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    LOGGER.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(
+        None, context.error, context.error.__traceback__
+    )
+    tb = "".join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    message = (
+        "An exception was raised while handling an update\n"
+        "<pre>update = {}</pre>\n\n"
+        "<pre>{}</pre>"
+    ).format(
+        html.escape(json.dumps(update.to_dict(), indent=2, ensure_ascii=False)),
+        html.escape(tb),
+    )
+
+    if len(message) >= 4096:
+        message = message[:4096]
+    # Finally, send the message
+    context.bot.send_message(chat_id=OWNER_ID, text=message, parse_mode=ParseMode.HTML)
+
+
+# for test purposes
+def error_callback(update: Update, context: CallbackContext):
+    error = context.error
+    try:
+        raise error
+    except Unauthorized:
+        print("no nono1")
+        print(error)
+        # remove update.message.chat_id from conversation list
+    except BadRequest:
+        print("no nono2")
+        print("BadRequest caught")
+        print(error)
+
+        # handle malformed requests - read more below!
+    except TimedOut:
+        print("no nono3")
+        # handle slow connection problems
+    except NetworkError:
+        print("no nono4")
+        # handle other connection problems
+    except ChatMigrated as err:
+        print("no nono5")
+        print(err)
+        # the chat_id of a group has changed, use e.new_chat_id instead
+    except TelegramError:
+        print(error)
+        # handle all other telegram related errors
+
+
+@run_async
+def help_button(update, context):
+    query = update.callback_query
+    mod_match = re.match(r"help_module\((.+?)\)", query.data)
+    prev_match = re.match(r"help_prev\((.+?)\)", query.data)
+    next_match = re.match(r"help_next\((.+?)\)", query.data)
+    back_match = re.match(r"help_back", query.data)
+
+    print(query.message.chat.id)
+
+    try:
+        if mod_match:
+            module = mod_match.group(1)
+            text = (
+                "Here is the help for the *{}* module:\n".format(
+                    HELPABLE[module].__mod_name__
+                )
+                + HELPABLE[module].__help__
+            )
+            query.message.edit_text(
+                text=text,
+                parse_mode=ParseMode.MARKDOWN,
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup(
+                    [[InlineKeyboardButton(text="Back", callback_data="help_back")]]
+                ),
+            )
+
+        elif prev_match:
+            curr_page = int(prev_match.group(1))
+            query.message.edit_text(
+                text=HELP_STRINGS,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=InlineKeyboardMarkup(
+                    paginate_modules(curr_page - 1, HELPABLE, "help")
+                ),
+            )
+
+        elif next_match:
+            next_page = int(next_match.group(1))
+            query.message.edit_text(
+                text=HELP_STRINGS,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=InlineKeyboardMarkup(
+                    paginate_modules(next_page + 1, HELPABLE, "help")
+                ),
+            )
+
+        elif back_match:
+            query.message.edit_text(
+                text=HELP_STRINGS,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=InlineKeyboardMarkup(
+                    paginate_modules(0, HELPABLE, "help")
+                ),
+            )
+
+        # ensure no spinny white circle
+        context.bot.answer_callback_query(query.id)
+        # query.message.delete()
+
+    except BadRequest:
+        pass
+
+
+@run_async
+def yone_about_callback(update, context):
+    query = update.callback_query
+    if query.data == "yone_":
+        query.message.edit_text(
+            text="""*INNEXIA* it's online since 29 March 2021 and it's constantly updated!
+                  \n*Bot Admins*
+                  \n• @SelfisXD, bot creator and main developer
+                  \n• The Doctor, server manager and developer
+                  \n• Manuel 5, developer
+                  \n*Supporter*
+                  \n[Click here](t.me/INSupporters) to consult the updated list of Official Supporters of the bot.
+                  \n• Thanks to all our *donors* for supporting server and development expenses and all those who have reported bugs or suggested new features.
+                  \n• We also thank *all the groups* who rely on our Bot for this service, we hope you will always like it: we are constantly working to improve it!""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
